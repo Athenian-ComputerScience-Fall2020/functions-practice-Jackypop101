8efd5821cde716abda395ebb702680f3edc7fb47
Jackypop101 <23hchen@athenian.org>
2020-09-15 20:58:53 +0000

    Finished Calculator

--- calculator.py
+++ calculator.py
@@ -2,33 +2,33 @@
 Collaborators: 
 
 '''
-def add():
-    print(num1 + num2)
+def add(num1,num2):
+    return num1 + num2
 
-def subtract():
-    print(num1 - num2)
+def subtract(num1,num2):
+    return num1 - num2
 
-def multiply():
-    print(num1 * num2)
+def multiply(num1,num2):
+    return num1 * num2
 
-def divide():
-    print(num1 / num2)
+def divide(num1,num2):
+    return num1 / num2
 
 option = int(input("Input 1 for addition, input 2 for subtraction, input 3 for multiplication, input 4 for division."))
 num1 = int(input("Input first number"))
 num2 = int(input("Input second number"))
 
 if option == (1):
-    add()
+    print(add(num1,num2))
         
 elif option == (2):
-    subtract()
+    print(subtract(num1,num2))
 
 elif option == (3):
-    multiply()
+    print(multiply(num1,num2))
        
 elif option == (4):
-    divide()
+    print(divide(num1,num2))
        
 
 
